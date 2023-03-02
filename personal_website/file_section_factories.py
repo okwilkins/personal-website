32c64cccacd7e2896904bb2d0dc92aeee592ec82
Oliver Wilkins <okwilkins@googlemail.com>
2023-03-01 18:21:50 -0700

    Further factory bug fixes and improvements

--- personal_website/file_section_factories.py
+++ personal_website/file_section_factories.py
@@ -21,7 +21,7 @@ class HeaderFactory(FileSectionFactory):
         header_end_str: str,
         key_value_sep: str
     ) -> Header:
-        header_lines = HeaderFactory._extract_section(lines, header_end_str)
+        header_lines = HeaderFactory._extract_section(lines, header_start_str, header_end_str)
 
         return Header(
             lines=header_lines,
@@ -31,7 +31,11 @@ class HeaderFactory(FileSectionFactory):
         )
 
     @staticmethod
-    def _extract_section(lines: list[str], header_end_str: str) -> list[str]:
+    def _extract_section(
+        lines: list[str],
+        header_start_str: Optional[str],
+        header_end_str: str
+    ) -> list[str]:
         '''
         Returns the list of strings that represent a file.
 
@@ -40,9 +44,19 @@ class HeaderFactory(FileSectionFactory):
             header_end_str: The string that indicates the end of a header.
         '''
         header_lines = []
+        header_started = False
+
+        # If the header start str is None, it means each line can
+        # be read as the header immediately 
+        if header_started is None:
+            header_started = True
 
         for line in lines:
-            if header_end_str in line:
+            if header_start_str == line and header_start_str is not None:
+                header_started = True
+                continue
+
+            if header_end_str == line and header_started:
                 break
 
             header_lines.append(line)
