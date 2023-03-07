6f915e1a58b24fa1ef1fc3e71415ad93a1adef19
Oliver Wilkins <okwilkins@googlemail.com>
2023-03-07 12:51:54 -0700

    Knowlege Graph

---
+++ personal_website/knowledge_graph.py
@@ -0,0 +1,118 @@
+from glob import glob
+from file_sections import Header
+from file_section_factories import HeaderFactory
+from themes import TerminalThemeZettleField
+from string_processing import case_to_camel_case
+from pathlib import Path
+from ast import literal_eval
+import json
+
+
+def get_links(header: Header) -> list[str]:
+    # Convert keys to snake_case
+    file_header_data = header.get_data()
+
+    links = []
+    keys = [
+        case_to_camel_case(str(TerminalThemeZettleField.ZETTELCASTEN_TAGS), sep='_'),
+        case_to_camel_case(str(TerminalThemeZettleField.SEQUENCE), sep='_')
+    ]
+
+    for key in keys:
+        key_links = file_header_data.get(str(key))
+
+        if key_links is not None:
+            key_links = literal_eval(key_links)
+        else:
+            key_links = []
+
+        for link in key_links:
+            links.append(link)
+    
+    return links
+
+
+def main(path: str) -> None:
+    # Read in file
+    with open(path, 'r') as f:
+        file_lines = f.readlines()
+
+    file_lines = [line.strip() for line in file_lines]
+
+    header = HeaderFactory.get_section(
+        lines=file_lines,
+        header_start_str='+++',
+        header_end_str='+++',
+        key_value_sep=' = '
+    )
+
+    links = get_links(header)
+
+    return {
+        'id': Path(path).stem,
+        'description': Path(path).stem,
+        'links': set(links)
+    }
+
+
+if __name__ == '__main__':
+    folders = ['map-of-content', 'slip-box']
+    data = {'nodes': [], 'links': []}
+    groups = [
+        Path(file).stem
+        for file in glob(f'./content/knowledge-system/map-of-content/*.md')
+    ]
+
+    files = []
+    node_size = {}
+
+    for folder in folders:
+        for file in glob(f'./content/knowledge-system/{folder}/*.md'):
+            files.append(file)
+
+    for file in files:
+        file_data = main(path=file)
+        group = None
+
+        if file_data['id'] in groups:
+            group = file_data['id']
+
+        for link in file_data['links']:
+            data['links'].append({
+                'source': file_data['id'],
+                'target': link
+            })
+
+            if (link in groups) and (group is None):
+                group = link
+            
+            if link in node_size:
+                node_size[link] += 1
+            else:
+                node_size[link] = 0
+
+        data['nodes'].append({
+            'id': file_data['id'],
+            'description': file_data['description'],
+            'group': group
+        })
+    
+    for node in data['nodes']:
+        if node['id'] not in node_size:
+            node_size[node['id']] = 0
+    
+    max_node_size = max(node_size.values())
+    
+    for node in data['nodes']:
+        # +1 as if the size is 0, it will have a size of 1
+        size = (node_size[node['id']] + 1) / max_node_size
+        
+        # Clamp value
+        if size < 0.1:
+            size = 0.1
+
+        node['node_size'] = size
+
+    
+    with open('static/graph-data.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
---
+++ static/js/graph.js
@@ -0,0 +1,129 @@
+const fetchJSONFile = (path) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", path, false);
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      const status = xhr.status;
+
+      if (status === 0 || (status >= 200 && status < 400)) {
+        console.log(`Retrieved ${path}!`);
+      } else {
+        console.log(`Could not retrieve ${path}!`);
+      }
+    }
+  };
+
+  xhr.send();
+  return JSON.parse(xhr.responseText);
+};
+
+let data = fetchJSONFile("/graph-data.json");
+
+// cross-link node objects
+data.links.forEach((link) => {
+  const a = data.nodes.filter(function (node) {
+    return node.id == link.source;
+  })[0];
+
+  const b = data.nodes.filter(function (node) {
+    return node.id == link.target;
+  })[0];
+
+  !a.neighbors && (a.neighbors = []);
+  !b.neighbors && (b.neighbors = []);
+  a.neighbors.push(b);
+  b.neighbors.push(a);
+
+  !a.links && (a.links = []);
+  !b.links && (b.links = []);
+  a.links.push(link);
+  b.links.push(link);
+});
+
+const elem = document.getElementById("graph");
+const NODE_R = 8;
+const highlightNodes = new Set();
+const highlightLinks = new Set();
+let hoverNode = null;
+const width = document.getElementsByClassName("post")[0].clientWidth;
+const post_title = document.getElementsByClassName("post-title")[0].textContent;
+
+const Graph = ForceGraph()(elem)
+  .graphData(data)
+  .width(width)
+  .height(300)
+  .nodeLabel("id")
+  .nodeAutoColorBy("group")
+  .nodeVal("node_size")
+  .onNodeClick((node) => {
+    // Center/zoom on node
+    Graph.centerAt(node.x, node.y, 1000);
+    Graph.zoom(3, 2000);
+  })
+  .nodeRelSize(NODE_R)
+  .onNodeHover(node => {
+    highlightNodes.clear();
+    highlightLinks.clear();
+    if (node) {
+      highlightNodes.add(node);
+      node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
+      node.links.forEach(link => highlightLinks.add(link));
+    }
+
+    hoverNode = node || null;
+  })
+  .onLinkHover(link => {
+    highlightNodes.clear();
+    highlightLinks.clear();
+
+    if (link) {
+      highlightLinks.add(link);
+      highlightNodes.add(link.source);
+      highlightNodes.add(link.target);
+    }
+  })
+  .autoPauseRedraw(false) // keep redrawing after engine has stopped
+  .linkWidth((link) => (highlightLinks.has(link) ? 5 : 1))
+  .linkDirectionalParticles(2)
+  .linkDirectionalParticleWidth((link) => (highlightLinks.has(link) ? 4 : 2))
+  .linkColor(() => "rgba(255,255,255,0.2)")
+  .nodeCanvasObjectMode((node) =>
+    highlightNodes.has(node) ? "before" : undefined
+  )
+  .nodeCanvasObject((node, ctx) => {
+    // add ring just for highlighted nodes
+    const min_node_size = 0.1;
+    const max_node_size = 1;
+
+    const min_graph_size = NODE_R * 0.5;
+    const max_graph_size = NODE_R * 1.4;
+
+    // Scale value from one the node sizes from Python to the
+    // range dictated by forcegraph
+    let node_size =
+      ((node.node_size - min_node_size) / (max_node_size - min_node_size)) *
+        (max_graph_size - min_graph_size) +
+      min_graph_size;
+
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, node_size, 0, 2 * Math.PI, false);
+    ctx.fillStyle = node === hoverNode ? "red" : "orange";
+    ctx.fill();
+  });
+
+  if (post_title) {
+    const title_node = data.nodes.filter(function (node) {
+      return node.id == post_title.trim();
+    })[0]
+
+    if (title_node) {
+      highlightNodes.clear();
+      highlightLinks.clear();
+      highlightNodes.add(title_node);
+      title_node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
+      title_node.links.forEach(link => highlightLinks.add(link));
+  
+      hoverNode = title_node || null;
+    }
+  }
